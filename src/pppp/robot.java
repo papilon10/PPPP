224faa27eb15d6395b8745fcd2a462019e24e21d
papilon10 <salvigna17@gmail.com>
2025-01-31 19:08:00 -0600

    robot putoo

---
+++ src/pppp/robot.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pppp;
+
+import java.awt.Color;
+import java.util.Date;
+
+/**
+ *
+ * @author Apple
+ */
+public class robot {
+    String nombre;
+    Color color;
+    Date fecha_creacion;
+    
+}
